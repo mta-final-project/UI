f51ff0158b934c4e56e02bbeea5b943bda00762b
Tal Alon <talalon369@gmail.com>
2024-09-07 14:21:30 +0300

    removed hardcoded api url in users service added an axios interceptor

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,15 @@
 import axios from "axios";
 import { FILES_API_URL, USERS_API_URL } from "./env";
 
+const authInterceptor = (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token !== null) {
+        console.log(`sending request with token`);
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}
+
 export const filesApi = axios.create({
     baseURL: FILES_API_URL
 })
@@ -9,3 +18,5 @@ export const usersApi = axios.create({
     baseURL: USERS_API_URL
 })
 
+filesApi.interceptors.request.use(authInterceptor);
+usersApi.interceptors.request.use(authInterceptor);
